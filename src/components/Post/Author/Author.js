a2ae29b40183763cdc1da8a856a7e681241cbc26
antonsirocka <anton.sirocka@mpd-group.com>
2020-08-31 21:16:52 +0100

    lint errors

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -2,11 +2,9 @@
 import React from 'react';
 // import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
-import { useSiteMetadata } from '../../../hooks';
+// import { useSiteMetadata } from '../../../hooks';
 
 const Author = () => {
-  // const { author } = useSiteMetadata();
-
   return (
     <div className={styles['author']}>
       {/* <p className={styles['author__bio']}>
