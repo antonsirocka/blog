3a8161f31270eb360a0e0266977f81b982c6f86a
antonsirocka <anton.sirocka@mpd-group.com>
2020-08-31 20:01:17 +0100

    lint errors

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -21,7 +21,7 @@ const Feed = ({ edges }: Props) => (
           <span className={styles['feed__item-meta-category']}>
             <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>
           </span>
-        </div>   
+        </div>
         <h2 className={styles['feed__item-title']}>
           <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
         </h2>
